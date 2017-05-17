d8cdf929f90a7b9450c993e7e09970a306bc9e50
NEH <erik@bodigiti.com>
2017-05-17 15:17:35 -0400

    Route: set up first route /core-warranty.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/index.css';
 
+import { Router, Route, browserHistory } from 'react-router';
+
 ReactDOM.render(
-  <App />,
+  <Router path="/" history={browserHistory}>
+    <Route path="/core-warranty" component={App} />
+  </Router>,
   document.getElementById('root')
 );
