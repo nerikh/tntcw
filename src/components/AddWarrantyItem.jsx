ba3dc10190fcc624b7e50edb05233b352b1caa72
NEH <erik@bodigiti.com>
2017-05-18 17:36:40 -0400

    Through WarrantyTracker Form up to point found Quantity is having problem.

--- src/components/AddWarrantyItem.jsx
+++ src/components/AddWarrantyItem.jsx
@@ -6,58 +6,176 @@ class AddWarrantyItem extends Component {
     super(props);
     this.state = {
       inputForm: {
-        Location: '',
+        Location: 'choose',
+        TurnedInBy: 'choose',
+        ReceivedBy: 'choose',
+        RO: '',
         VIN: '',
-        Invoice: '',
+        Vendor: '',
+        Customer: '',
+        FailedPartNumber: '',
+        Quantity: 1,
+        Description: ''
       },
       formInput: true,
       formOutput: false
     }
+    this.baseState = this.state
   }
+ 
+ 
+  submitForm() {
+    const { Location, TurnedInBy, ReceivedBy, RO, VIN, Vendor, Customer, FailedPartNumber, Quantity, Description  } = this.state;
 
-  addWarrantyItem() {
-    const { Location, VIN, Invoice } = this.state;
-
-    if ( Location !== 'choose' && VIN !== '' && Invoice !== '') {
-      warrantyItemRef.push({ Location, VIN, Invoice });
+    if ( (Location !== 'choose' && TurnedInBy !== 'choose' && ReceivedBy !== 'choose') && RO !== '' && VIN !== '' && Vendor !== '' && Customer !== '' && FailedPartNumber !== '' && Description !== '') {
+      warrantyItemRef.push({ Location, TurnedInBy, ReceivedBy, RO, VIN, Vendor, Customer, FailedPartNumber, Quantity, Description });
     }
     console.log('Add Warranty Item State', this.state);
+  }
 
+  resetForm = () => {
+  const { Location, TurnedInBy, ReceivedBy, VIN, Invoice } = this.state;
+
+    return ( (Location && TurnedInBy && ReceivedBy === 'choose') && VIN === '' && Invoice === '');
   }
 
   render() {
+    let input;
+
     return (
       <form>
         <div className="row">
-          <div className="small-12 medium-8 large-10 columns">
-            <select 
-              onChange={event => this.setState({Location: event.target.value})}
-            >
-            <option value="choose">Select Location</option>
-            <option value="JAX">JAX</option>
-            <option value="NFWS">NFWS</option>
-            <option value="LC">LC</option>
-            <option value="WC">WC</option>
-            <option value="440">440</option>
-          </select>
-          <input
-              type="text"
-              placeholder="VIN"
-              className=""
-              onChange={event => this.setState({VIN: event.target.value})}
-            />
-            <input
-              type="text"
-              placeholder="Invoice"
-              className=""
-              onChange={event => this.setState({Invoice: event.target.value})}
-            />
+          <div className="small-12 medium-8 large-6 columns">
+            <div className="input-group">
+              <span className="input-group-label">Branch</span>
+              <select 
+                onChange={event => this.setState({Location: event.target.value})}
+                className="input-group-field"
+              >
+                <option value="choose">Select Location</option>
+                <option value="JAX">JAX</option>
+                <option value="NFWS">NFWS</option>
+                <option value="LC">LC</option>
+                <option value="WC">WC</option>
+                <option value="440">440</option>
+              </select>
+            </div>
+            <div className="input-group">
+              <span className="input-group-label">Turned In By</span>
+              <select 
+                onChange={event => this.setState({TurnedInBy: event.target.value})}
+                className="input-group-field"
+              >
+                <option value="choose">Select Name</option>
+                <option value="0001-Billy">Billy</option>
+                <option value="0002-Bobby">Bobby</option>
+                <option value="0003-Mike">Mike</option>
+                <option value="0004-John">John</option>
+                <option value="0005-Paul">Paul</option>
+              </select>
+            </div>
+            <div className="input-group">
+              <span className="input-group-label">Received By</span>
+              <select 
+                onChange={event => this.setState({ReceivedBy: event.target.value})}
+                className="input-group-field"
+              >
+                <option value="choose">Select Name</option>
+                <option value="0001-Billy">Billy</option>
+                <option value="0002-Bobby">Bobby</option>
+                <option value="0003-Mike">Mike</option>
+                <option value="0004-John">John</option>
+                <option value="0005-Paul">Paul</option>
+              </select>
+            </div>
+            <div className="input-group">
+              <span className="input-group-label">RO</span>
+              <input
+                type="text"
+                placeholder="RO"
+                className="input-group-field"
+                onChange={event => this.setState({RO: event.target.value})}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-label">VIN</span>
+              <input
+                  type="text"
+                  placeholder="VIN"
+                  className="input-group-field"
+                  onChange={event => this.setState({VIN: event.target.value})}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-label">Vendor</span>
+              <input
+                type="text"
+                placeholder="Vendor"
+                className="input-group-field"
+                onChange={event => this.setState({Vendor: event.target.value})}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-label">Customer</span>
+              <input
+                type="text"
+                placeholder="Customer"
+                className="input-group-field"
+                onChange={event => this.setState({Customer: event.target.value})}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-label">FailedPartNumber</span>
+              <input
+                type="text"
+                placeholder="FailedPartNumber"
+                className="input-group-field"
+                onChange={event => this.setState({FailedPartNumber: event.target.value})}
+              />
+            </div>
+            <div className="input-group">
+              <span className="input-group-label">Quantity</span>
+              <select 
+                onChange={event => this.setState({Quantity: event.target.value})}
+                className="input-group-field"
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="6">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+            <div>
+              <div 
+                className="input-group-label input-group-field"
+                style={{textAlign: 'left'}}
+              >Description
+              </div>
+              <textarea
+                  placeholder="Description"
+                  onChange={event => this.setState({Description: event.target.value})}
+                >
+              </textarea>
+            </div>
             <button
               className="button primary"
               type="button"
-              onClick={() => this.addWarrantyItem()}
+              onClick={() => this.submitForm()}
             >
-              Submit
+              Create
+            </button>
+            <button
+              className="button primary hollow"
+              type="button"
+              onClick={() => this.resetForm()}
+            >
+              Reset
             </button>
           </div>
         </div>
